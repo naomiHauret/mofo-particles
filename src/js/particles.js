05db583f2662e2023908a761bbb4acb3ba529ef8
Naomi Hauret <naomi@troopers.email>
2017-09-21 16:48:15 +0200

    updated src files

--- src/js/particles.js
+++ src/js/particles.js
@@ -1,36 +1,47 @@
 class Particle {
-    constructor() {
+    constructor(canvas) {
         this.x = Math.random() * 5 + 5,
-            this.y = Math.random() * 5 + 5,
-            this.width = Math.random() * canvas.width
-        this.height = Math.random() * canvas.height
+        this.y = Math.random() * 5 + 5,
+        this.width = 10,
+        this.height = 10,
         this.speedX = Math.random() > 0.5 ? Math.random() : -Math.random(),
-            this.speedY = Math.random() > 0.5 ? Math.random() : -Math.random(),
-            this.angleX = Math.random() > 0.5 ? Math.random() : -Math.random(),
-            this.angleY = Math.random() > 0.5 ? Math.random() : -Math.random()
+        this.speedY = Math.random() > 0.5 ? Math.random() : -Math.random(),
+        this.angleX = Math.random() > 0.5 ? Math.random() : -Math.random(),
+        this.angleY = Math.random() > 0.5 ? Math.random() : -Math.random(),
+        this.color = this.randomColor()
     }
 
-    update() {
+    update(canvas) {
         this.x += this.speedX + this.angleX
         this.y += this.speedY + this.angleY
 
         if (this.x < 0 || this.x + this.width > canvas.width) {
             const sign = this.x < 0 ? 1 : -1
             this.angleX = Math.random() * 2 * sign
-            this.speedX = -speedX
+            this.speedX = -this.speedX
         }
 
         if (this.y < 0 || this.y + this.width > canvas.width) {
             const sign = this.y < 0 ? 1 : -1
             this.angleY = Math.random() * 2 * sign
-            this.speedY = -speedY
+            this.speedY = -this.speedY
         }
     }
 
-    draw() {
-        ctx.fillStyle = "red"
-        ctx.fillRect(x, y, width, height)
+    draw(ctx) {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    randomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 }
+
 
 export default Particle 
--- src/main.js
+++ src/main.js
@@ -3,19 +3,26 @@ import Particle from "./js/particles"
 
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext("2d")
-const number = 80
+const number = 800
 const particles = []
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-for(i= 0; i < number; i++) particles.push(new Particle())
+
+function repaint(canvas) {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+}
+
+repaint(canvas)
+window.addEventListener('resize', repaint(canvas))
+
+for(let i= 0; i < number; i++) particles.push(new Particle(canvas))
 
     const animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     
     particles.forEach((particle) =>{
-        particle.draw()
-        particle.update()
+        particle.draw(ctx)
+        particle.update(canvas)
     })
     requestAnimationFrame(animate)
 }
